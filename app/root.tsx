7fc2c202e429471375d3283defd93aa9e5a80474
roblesdotdev <httpsnip@gmail.com>
2024-10-14 18:46:18 -0300

    remove unused boilerplate

--- app/root.tsx
+++ app/root.tsx
@@ -5,22 +5,15 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node";
+import type { MetaFunction } from "@remix-run/node";
+import "~/styles/global.css";
 
-import "./tailwind.css";
-
-export const links: LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
