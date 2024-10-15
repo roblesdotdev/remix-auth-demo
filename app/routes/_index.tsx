699c04430c9b264b812ea214d3e842a471bf63ae
roblesdotdev <httpsnip@gmail.com>
2024-10-14 21:14:05 -0300

    setup prisma

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,19 @@
+import { json, useLoaderData } from '@remix-run/react'
+import { db } from '~/utils/db.server'
+
+export async function loader() {
+  const issues = await db.issue.count()
+  return json({
+    issues,
+  })
+}
+
 export default function Index() {
+  const data = useLoaderData<typeof loader>()
+
   return (
     <div className="grid h-screen place-items-center">
-      <h1>Working...</h1>
+      <h1>Current issues {data.issues}</h1>
     </div>
   )
 }
---
+++ app/utils/db.server.ts
@@ -0,0 +1,5 @@
+import { PrismaClient } from '@prisma/client'
+import { singleton } from './singleton.server'
+
+// hard-code a unique key so we can look up the client when this module gets re-imported
+export const db = singleton('prisma', () => new PrismaClient())
