4c2a570d652ac84b560be81dcbe9f2491651b7b4
roblesdotdev <httpsnip@gmail.com>
2024-10-14 23:11:56 -0300

    setup remix flat routes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { vitePlugin as remix } from '@remix-run/dev'
 import { remixDevTools } from 'remix-development-tools'
+import { flatRoutes } from 'remix-flat-routes'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   plugins: [
     remixDevTools(),
     remix({
@@ -11,6 +15,25 @@ export default defineConfig({
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
+      },
+      ignoredRouteFiles: ['**/*'],
+      serverModuleFormat: 'esm',
+      routes: async defineRoutes => {
+        return flatRoutes('routes', defineRoutes, {
+          ignoredRouteFiles: [
+            '.*',
+            '**/*.css',
+            '**/*.test.{js,jsx,ts,tsx}',
+            '**/__*.*',
+            // This is for server-side utilities you want to colocate
+            // next to your routes without making an additional
+            // directory. If you need a route that includes "server" or
+            // "client" in the filename, use the escape brackets like:
+            // my-route.[server].tsx
+            '**/*.server.*',
+            '**/*.client.*',
+          ],
+        })
       },
     }),
     tsconfigPaths(),
